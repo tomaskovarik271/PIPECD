2812047b810defb004543956e0d52cf14b378f80
tomaskovarik271 <64414613+tomaskovarik271@users.noreply.github.com>
2025-06-03 00:22:36 +0200

    chore: Remove seeding_sql.txt to resolve GitHub secret scanning alert

---
+++ frontend/.vite/deps/@supabase_auth-ui-shared.js
@@ -0,0 +1,31 @@
+import {
+  CLASS_NAMES,
+  PREPENDED_CLASS_NAMES,
+  SocialLayouts,
+  ThemeMinimal,
+  ThemeSupa,
+  VIEWS,
+  darkThemes,
+  en_default,
+  generateClassNames,
+  merge,
+  minimal,
+  supabase,
+  template
+} from "./chunk-OH4ZLUJL.js";
+import "./chunk-EWTE5DHJ.js";
+export {
+  CLASS_NAMES,
+  PREPENDED_CLASS_NAMES,
+  SocialLayouts,
+  ThemeMinimal,
+  ThemeSupa,
+  VIEWS,
+  darkThemes,
+  en_default as en,
+  generateClassNames,
+  merge,
+  minimal,
+  supabase,
+  template
+};
